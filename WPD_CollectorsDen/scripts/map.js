3ff137661998e9d8878349fff5a0790fe96b7a77
Hariisah Bahrin <riisuriisu80@gmail.com>
2017-04-05 09:25:30 +0800

    successfully added marker on map

--- WPD_CollectorsDen/scripts/map.js
+++ WPD_CollectorsDen/scripts/map.js
@@ -3,7 +3,13 @@
     center: new google.maps.LatLng(4.885728, 114.931669),
     zoom: 20,
     };
+
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(4.885728, 114.931669),
+        map: map,
+    });
 
   }
       
